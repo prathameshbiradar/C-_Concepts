8c2d9bc7e3fe00f88d9f41571ca54a599c2f9bac
prathmeshbiradar <psbiradar948@gmail.com>
2024-10-11 15:10:11 +0530

    Destructor programs

---
+++ Destructor/Destructor1.cs
@@ -0,0 +1,34 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Destructor
+{
+    public class Demo {
+
+        public Demo() {
+            Console.WriteLine("Constructor is called");
+        }
+        
+        ~Demo() {
+            Console.WriteLine("Obj is destroyed ");
+        }
+    
+    }
+    internal class Destructor1
+    {
+        static void Main(string[] args)
+        {
+           
+            Demo obj = new Demo();
+            
+            obj = null;
+            
+
+        }
+    }
+}
+*/
---
+++ Destructor/Destructor2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Destructor
+{
+
+    public class A {
+         ~A() {
+            Console.WriteLine("Class A object is destroyed");
+        }
+    }
+    public class B : A {
+        ~B() {
+            Console.WriteLine("Class B object is Destroyed");
+        }
+    }
+    public class C : B 
+    {
+        ~C() {
+            Console.WriteLine("Class C object is Destroyed");
+        }
+    }
+   
+    public class Destructor2
+    {
+        static void Main(String[] args) {
+
+            C obj = new C();
+            obj = null;
+            GC.Collect();
+
+        }
+
+
+    }
+}

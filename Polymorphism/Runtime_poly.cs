b7289c02c501c327347cb844319c23998c2f552a
prathmeshbiradar <psbiradar948@gmail.com>
2024-10-11 15:11:25 +0530

    Remaining Polymorphism Programs

--- Polymorphism/Runtime_poly.cs
+++ Polymorphism/Runtime_poly.cs
@@ -1,4 +1,4 @@
-﻿/*
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,4 +31,3 @@ namespace Polymorphism
         }
     }
 }
-*/
--- Polymorphism/Sealed_keyword.cs
+++ Polymorphism/Sealed_keyword.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/*
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,26 @@ using System.Threading.Tasks;
 
 namespace Polymorphism
 {
-    internal class Sealed_keyword
+    // if we use sealed keyword before class then it can't be derived and if we use before method then it can't ovrride it.
+    public sealed  class School {
+
+        public School() {
+
+            Console.WriteLine("Name of Every school is different");
+        }
+        public sealed virtual void  no_of_Students (){
+
+            }
+    }
+    public class Nmc : School {
+
+        public override void no_of_Students() {
+
+            Console.WriteLine("There are 400 students in the school");
+        }
+    }
+    public class Sealed_keyword
     {
     }
 }
+*/

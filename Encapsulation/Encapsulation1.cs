a19d9a06b3bb88afcdc813591306f00c950947ef
prathmeshbiradar <psbiradar948@gmail.com>
2024-10-11 15:08:36 +0530

    Added encapsulation programs

---
+++ Encapsulation/Encapsulation1.cs
@@ -0,0 +1,35 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Encapsulation1
+    {
+        private double amount;
+
+        public double getAmount()
+        {
+        
+            return amount;
+        }
+        public void setAmount(double amount) { 
+            
+            this.amount = amount;
+
+        }
+        static void Main(string[] args)
+        {
+            Encapsulation1 obj = new Encapsulation1();
+            obj.setAmount(1000);
+            double amount = obj.getAmount();
+            Console.WriteLine("The average salary of an employee is: "+amount);
+
+        }
+    }
+}
+*/
---
+++ Encapsulation/Encapsulation2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+
+    public class Encapsulation2
+    {
+        private String name;
+        public String _name
+        {
+            get {
+
+                return name;
+            }
+            set { 
+            
+                name = value;
+            }
+        }
+
+
+        static void Main(String[] args)
+        {
+        
+            Encapsulation2 obj = new Encapsulation2();
+            obj._name = "Aniket";
+            Console.WriteLine("The name of the Employee is: "+obj._name);
+        }
+
+    }
+}
